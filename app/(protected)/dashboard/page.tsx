bfbbb363a62a9288617c0e6b14d6dc193f7ecaa7
Pranay <contactpranay10@gmail.com>
2026-03-09 15:29:20 +0530

    `feat: Add notebook renaming feature with inline editing`

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { getGreeting } from "@/app/utils/getGreeting";
 import Link from "next/link";
 import { Plus, MoreVertical, Pencil, Share, Trash } from "lucide-react";
 import CreateNotebookModal from "@/app/components/CreateNotebookModal";
+import toast from "react-hot-toast";
 
 type Notebook = {
   id: string;
@@ -22,6 +23,8 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [openModal, setOpenModal] = useState(false);
   const [openMenu, setOpenMenu] = useState<string | null>(null);
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [editValue, setEditValue] = useState("");
 
   useEffect(() => {
     const fetchNotebooks = async () => {
@@ -39,6 +42,8 @@ export default function Dashboard() {
 
     fetchNotebooks();
   }, []);
+
+
 
   return (
     <div className="max-w-6xl mx-auto py-12 px-6 text-white">
@@ -99,8 +104,47 @@ export default function Dashboard() {
           {notebooks.map((notebook) => (
             <div className="bg-[#181818] border border-zinc-800 rounded-xl p-5 hover:border-zinc-600 hover:bg-[#1f1f1f] transition relative">
 
-              <Link href={`/notebook/${notebook.id}`}>
-                <h3 className="text-lg font-semibold mt-1">{notebook.name}</h3>
+              <Link
+                href={`/notebook/${notebook.id}`}
+                onClick={(e) => {
+                  if (editingId === notebook.id) {
+                    e.preventDefault();
+                  }
+                }}
+              >
+                {editingId === notebook.id ? (
+                  <input
+                    value={editValue}
+                    onChange={(e) => setEditValue(e.target.value)}
+                    onBlur={() => setEditingId(null)}
+                    onKeyDown={async (e) => {
+                      if (e.key === "Enter") {
+
+                        await fetch(`/api/notebook/${notebook.id}`, {
+                          method: "PATCH",
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                          body: JSON.stringify({ name: editValue }),
+                        });
+
+                        setNotebooks((prev) =>
+                          prev.map((n) =>
+                            n.id === notebook.id ? { ...n, name: editValue } : n
+                          )
+                        );
+
+                        toast.success("Notebook updated successfully!")
+
+                        setEditingId(null);
+                      }
+                    }}
+                    className="bg-transparent border border-zinc-700 rounded px-2 py-1 text-white w-full"
+                    autoFocus
+                  />
+                ) : (
+                  <h3 className="text-lg font-semibold">{notebook.name}</h3>
+                )}
 
                 {notebook.description && (
                   <p className="text-zinc-400 text-sm mt-2 line-clamp-2">
@@ -131,7 +175,15 @@ export default function Dashboard() {
               {openMenu === notebook.id && (
                 <div className="absolute right-1 top-10 bg-[#222] border border-zinc-700 rounded-lg shadow-lg w-40 py-1 z-50">
 
-                  <button className="w-full text-left px-4 py-2 hover:bg-zinc-700 text-sm">
+                  <button
+                    className="w-full text-left px-4 py-2 hover:bg-zinc-700 text-sm"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setEditingId(notebook.id);
+                      setEditValue(notebook.name);
+                      setOpenMenu(null);
+                    }}
+                  >
                     <div className="flex items-center gap-2">
                       <Pencil size={18} />
                       Rename
--- app/api/notebook/[id]/route.ts
+++ app/api/notebook/[id]/route.ts
@@ -29,3 +29,72 @@ export async function GET(request: NextRequest, { params }: { params: Promise<{ 
         return NextResponse.json({ error: "Failed to get notebook." }, { status: 500 });
     }
 };
+
+
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        const { session, error } = await requireAuth();
+        if (error) {
+            return NextResponse.json({ error }, { status: 401 });
+        };
+
+
+        const { id } = await params;
+
+        const notebook = await prisma.notebook.findFirst({
+            where: {
+                id,
+                userId: session.user?.id,
+            },
+        });
+
+        if (!notebook) {
+            return NextResponse.json(
+                { error: "Notebook not found" },
+                { status: 404 }
+            );
+        }
+
+        const { name, description } = await request.json();
+
+        if (!name && !description) {
+            return NextResponse.json(
+                { error: "Nothing to update" },
+                { status: 400 }
+            );
+        };
+
+
+        if (name.length > 100) {
+            return NextResponse.json(
+                { error: "Name cannot be more than 100 characters" },
+                { status: 400 }
+            )
+        };
+
+
+
+        if (description.length > 400) {
+            return NextResponse.json(
+                { error: "Bio cannot be more than 400 characters" },
+                { status: 400 }
+            )
+        };
+
+        const updatedNotebook = await prisma.notebook.update({
+            where: { id },
+            data: {
+                ...(name && { name }),
+                ...(description && { description }),
+            },
+        });
+
+
+        return NextResponse.json({ notebook: updatedNotebook }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to update notebook." },
+            { status: 500 }
+        );
+    }
+};
